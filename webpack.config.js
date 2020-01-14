da50a942e04dded73e6e450b3c4ae670f243bea9
sasaafanasienka <mark0182@tut.by>
2020-01-14 15:23:06 +0300

    change flickity to swiper js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -21,8 +21,25 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] //добавили минификацию CSS
-      }
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']     //добавили минификацию CSS  
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, 
+              disable: true, 
+            },
+          },
+        ],
+    },
+    {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+    }
     ]
   },
   plugins: [
@@ -30,9 +47,9 @@ module.exports = {
       filename: 'style.[contenthash].css',
       }),
     new HtmlWebpackPlugin({
-      inject: false,
-      template: './src/index.html',
-      filename: 'index.html'
+      inject: false, //стили не нужно прописывать внутри тегов
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после
     }),
     new WebpackMd5Hash()
   ]
